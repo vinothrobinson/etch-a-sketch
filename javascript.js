a123059addc3e2113e09ed9a35361555778c6059
vinothrobinson <vinothrobinson03@gmail.com>
2023-05-16 10:45:01 -0400

    Change the etching to be activated by click + holding the mouse down, and implement a function to modify the size of the grid

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 const container = document.querySelector("#etching-grid");
 
-function createGrid(){
-    for (let i = 0; i < 16; i++){
+function createGrid(dimension){
+    for (let i = 0; i < dimension; i++){
         const row = document.createElement("div");
         row.classList.add("row");
-        for (let j = 0; j < 16; j++){
+        for (let j = 0; j < dimension; j++){
             const grid = document.createElement("div");
             grid.classList.add("grid");
             row.append(grid);
@@ -13,16 +13,37 @@ function createGrid(){
     }
 }
 
-createGrid();
+createGrid(16);
 
 
 let grids = document.getElementsByClassName("grid");
 
-function hoverState(){
-    this.style.backgroundColor = "black";
+function hoverState(object){
+    object.style.backgroundColor = "black";
 }
+
+let isMouseDown = false;
 
 for (let i = 0; i < grids.length; i++){
-    grids[i].addEventListener('mouseover', hoverState);
+    grids[i].addEventListener('mousedown', () => {
+        isMouseDown = true;
+      });
+      
+    grids[i].addEventListener('mouseup', () => {
+        isMouseDown = false;
+      });
+      
+    grids[i].addEventListener('mousemove', (event) => {
+        if (isMouseDown) {
+          hoverState(grids[i]);
+        }
+      });
 }
 
+const newGrid = document.querySelector('button');
+newGrid.addEventListener('click', createNewGrid);
+
+function createNewGrid(){
+    let dimension = prompt("Choose how big you want the grid to be, the max size is 100: ");
+    createGrid(dimension);
+}
