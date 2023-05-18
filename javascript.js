a5cb2264e3fa068d2fa6e15a92e0f5e215a79151
vinothrobinson <vinothrobinson03@gmail.com>
2023-05-18 11:35:51 -0400

    Fix how the grid was created(size of initial grid does not change, only squares inside), and create the clearGrid() function

--- javascript.js
+++ javascript.js
@@ -1,13 +1,15 @@
 const DEFAULT_SIZE = 16;
-const container = document.querySelector("#etching-grid");
+let current_size = DEFAULT_SIZE
+let container = document.querySelector(".etching-grid");
 let isMouseDown = false;
 function createGrid(dimension){
-    for (let i = 0; i < dimension; i++){
-        const column = document.createElement("div");
-        column.classList.add("column");
-        for (let j = 0; j < dimension; j++){
-            const grid = document.createElement("div");
-            grid.classList.add("grid");
+    container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+    let max = dimension * dimension
+
+    for (let i = 0; i < max; i++){
+        let grid = document.createElement("div");
+        grid.classList.add("grid");
             grid.addEventListener('mousedown', () => {
                 isMouseDown = true;
             });
@@ -19,9 +21,7 @@ function createGrid(dimension){
                     hoverState(grid);
                 }
             });
-            column.append(grid);
-        }
-        container.append(column);
+        container.insertAdjacentElement('beforeend', grid);
     }
 }
 
@@ -36,9 +36,21 @@ newGrid.addEventListener('click', createNewGrid);
 
 function createNewGrid(){
     let dimension = prompt("Choose how big you want the grid to be, the max size is 100: ");
+    while (dimension > 100){
+        dimension = prompt("The number you chose was too big, please choose a number between 2 and 100: ");
+    }
     if (dimension !== ""){
+        current_size = dimension;
         container.replaceChildren();
         createGrid(dimension);
     }
     else return;
+}
+
+const clear = document.querySelector('#clear-grid');
+clear.addEventListener('click', clearGrid);
+
+function clearGrid(){
+    container.replaceChildren();
+    createGrid(current_size);
 }
